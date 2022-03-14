0edba774b2148289525e39104932e09fb048acf0
Helmi Nur Ihsan <helmi.n.ihsan@gmail.com>
2022-03-14 16:52:55 +0700

    add navigation and customer name input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -116,6 +116,8 @@ export default function Home({}: Props) {
   const [selectedItem, setSelectedItem] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [orders, setOrders] = useState([]);
+  const [tabIndex, setTabIndex] = useState(0);
+  const [selectedTab, setSelectedTab] = useState('customer');
 
   const handleConfirmItem = (item: any) => {
     const isExist = orders.filter((order) => {
@@ -133,82 +135,124 @@ export default function Home({}: Props) {
     }
   };
 
+  const handleTabSwitch = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setSelectedTab(event.currentTarget.getAttribute("data-tabs"));
+    console.log("Selected Tab:" + event.currentTarget.getAttribute("data-tabs"));
+
+    setTabIndex(+event.currentTarget.getAttribute("data-tabIndex"));
+    console.log("Selected Index:" + (+event.currentTarget.getAttribute("data-tabindex")));
+  }
   return (
     <div>
-      <div className="container bg-white shadow rounded-lg mt-16 p-8 flex gap-x-5 w-full">
-        <aside className="w-2/12 overflow-auto " style={{ maxHeight: '70vh' }}>
-          <ul className="list-none">
-            {datas.map(({ category }, index) => (
-              <li
-                key={index}
-                className={`p-3 first-letter:uppercase  cursor-pointer hover:text-red-100 transition-all duration-75 ${
-                  selectedCurrentIndex === index
-                    ? 'border rounded bg-gray-500 text-white hover:text-white'
-                    : ''
-                }`}
-                onClick={() => setSelectedCurrentIndex(index)}
-              >
-                {category}
-              </li>
-            ))}
-          </ul>
-        </aside>
-        <div
-          className="w-10/12  overflow-auto p-4 border-l"
-          style={{ maxHeight: '70vh', height: '70vh' }}
-        >
-          <div className="grid grid-cols-4 gap-4">
-            {datas[selectedCurrentIndex]?.items?.length > 0 ? (
-              datas[selectedCurrentIndex].items.map((item, index) => (
-                <div
-                  className="border rounded-md cursor-pointer hover:shadow-md relative"
-                  onClick={() => {
-                    setSelectedItem(item);
-                    setIsModalOpen(true);
-                  }}
-                  key={index}
-                >
-                  <figure className="bg-white rounded-md">
-                    <div className="relative">
-                      {orders.filter((order) => order.item.name === item.name)
-                        .length > 0 && (
-                        <div className="absolute w-10 h-10 z-10">
-                          <Img
-                            src="/images/icon-checklist.png"
-                            height="100%"
-                            width="100%"
-                            alt={item?.name ?? '-'}
-                            classname="object-cover rounded-tl-md rounded-tr-md"
-                          />
+      <div className="container bg-white shadow rounded-lg mt-16 w-2/3">
+        <ul id="tabs" className="inline-flex w-full px-1 pt-2 ">
+          <li className={"px-4 py-2 font-semibold text-gray-800 rounded-t opacity-50" + (tabIndex >= 0 ? "-mb-px border-b-4 border-blue-400" : "")}><a className='tabs-navigation' data-tabindex="0" data-tabs="customer" onClick={handleTabSwitch}>Customer Info</a></li>
+          <li className={"px-4 py-2 font-semibold text-gray-800 rounded-t opacity-50" + (tabIndex >= 1 ? "-mb-px border-b-4 border-blue-400" : "")}> &gt; </li>
+          <li className={"px-4 py-2 font-semibold text-gray-800 rounded-t opacity-50" + (tabIndex >= 1 ? "-mb-px border-b-4 border-blue-400" : "")}><a className='tabs-navigation' data-tabindex="1" data-tabs="tables" onClick={handleTabSwitch}>Choose Table</a></li>
+          <li className={"px-4 py-2 font-semibold text-gray-800 rounded-t opacity-50" + (tabIndex >= 2 ? "-mb-px border-b-4 border-blue-400" : "")}> &gt; </li>
+          <li className={"px-4 py-2 font-semibold text-gray-800 rounded-t opacity-50" + (tabIndex >= 2 ? "-mb-px border-b-4 border-blue-400" : "")}><a className='tabs-navigation' data-tabindex="2" data-tabs="menus" onClick={handleTabSwitch}>Main Menu</a></li>
+        </ul>
+      </div>
+      <div className="container bg-white shadow rounded-lg mt-4 p-8">
+        <div className={"flex justify-center "+ (selectedTab === "customer" ? "" : "hidden")} id="customerID">
+          <div className="w-full max-w-md">
+            <form className="bg-white rounded px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                  Welcome to Bee Cafe, what can we call you?
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Enter your name" />
+              </div>
+              <div className="flex items-center justify-between">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                  Let's go!
+                </button>
+              </div>
+            </form>
+            <p className="text-center text-gray-500 text-xs">
+              &copy;2020 Acme Corp. All rights reserved.
+            </p>
+          </div>
+        </div>
+        <div className={selectedTab === "tables" ? "" : "hidden"} id="tableNo">
+          <h1>Choose table section</h1>
+        </div>
+        <div className={"flex gap-x-5 w-full " + (selectedTab === "menus" ? "" : "hidden")} id="menuOrders">
+          <aside className="w-2/12 overflow-auto " style={{ maxHeight: '70vh' }}>
+              <ul className="list-none">
+                {datas.map(({ category }, index) => (
+                  <li
+                    key={index}
+                    className={`p-3 first-letter:uppercase  cursor-pointer hover:text-red-100 transition-all duration-75 ${
+                      selectedCurrentIndex === index
+                        ? 'border rounded bg-gray-500 text-white hover:text-white'
+                        : ''
+                    }`}
+                    onClick={() => setSelectedCurrentIndex(index)}
+                  >
+                    {category}
+                  </li>
+                ))}
+              </ul>
+          </aside>
+          <div
+            className="w-10/12  overflow-auto p-4 border-l"
+            style={{ maxHeight: '70vh', height: '70vh' }}
+          >
+            <div className="grid grid-cols-4 gap-4">
+              {datas[selectedCurrentIndex]?.items?.length > 0 ? (
+                datas[selectedCurrentIndex].items.map((item, index) => (
+                  <div
+                    className="border rounded-md cursor-pointer hover:shadow-md relative"
+                    onClick={() => {
+                      setSelectedItem(item);
+                      setIsModalOpen(true);
+                    }}
+                    key={index}
+                  >
+                    <figure className="bg-white rounded-md">
+                      <div className="relative">
+                        {orders.filter((order) => order.item.name === item.name)
+                          .length > 0 && (
+                          <div className="absolute w-10 h-10 z-10">
+                            <Img
+                              src="/images/icon-checklist.png"
+                              height="100%"
+                              width="100%"
+                              alt={item?.name ?? '-'}
+                              classname="object-cover rounded-tl-md rounded-tr-md"
+                            />
+                          </div>
+                        )}
+
+                        <Img
+                          src={item?.image}
+                          height="100%"
+                          width="100%"
+                          alt={item?.name ?? '-'}
+                          classname="object-cover rounded-tl-md rounded-tr-md"
+                        />
+
+                        <div className="meta">
+                          <p className="mt-3 px-3 text-lg text-white">
+                            IDR {formatRupiah(item?.price ?? 0)}
+                          </p>
                         </div>
-                      )}
-
-                      <Img
-                        src={item?.image}
-                        height="100%"
-                        width="100%"
-                        alt={item?.name ?? '-'}
-                        classname="object-cover rounded-tl-md rounded-tr-md"
-                      />
-
-                      <div className="meta">
-                        <p className="mt-3 px-3 text-lg text-white">
-                          IDR {formatRupiah(item?.price ?? 0)}
-                        </p>
                       </div>
-                    </div>
 
-                    <figcaption className="p-4 text-black text-center">
-                      <h2 className="text-gray-900 text-center text-sm">
-                        {item?.name ?? '-'}
-                      </h2>
-                    </figcaption>
-                  </figure>
-                </div>
-              ))
-            ) : (
-              <h2>Sorry, no item available</h2>
-            )}
+                      <figcaption className="p-4 text-black text-center">
+                        <h2 className="text-gray-900 text-center text-sm">
+                          {item?.name ?? '-'}
+                        </h2>
+                      </figcaption>
+                    </figure>
+                  </div>
+                ))
+              ) : (
+                <h2>Sorry, no item available</h2>
+              )}
+            </div>
           </div>
         </div>
       </div>
